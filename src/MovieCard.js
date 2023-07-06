7afb50c9a844e05ba2c661ea258caadd703627b7
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-06 15:47:58 -0700

    fix/rounded the movie rating up

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -3,13 +3,15 @@ import './MovieCard.css';
 const MovieCard = ({ movie }) => {
   const { title, poster_path, release_date, average_rating } = movie;
 
+  const roundedRating = Math.round(average_rating * 10) / 10;
+
   return (
     <div className="movie-card">
-      <img src={poster_path} alt={title} />
+      <img className="movie-poster" src={poster_path} alt={title} />
       <div className="movie-details">
         <h3>{title}</h3>
         <p>Year: {release_date.split("-")[0]}</p>
-        <p>Rating: {average_rating}</p>
+        <p>Rating: {roundedRating}</p>
       </div>
     </div>
   );
