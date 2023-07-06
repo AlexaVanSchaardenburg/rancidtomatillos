f6d3139c1220d55832aca128a9e38c9d78d0983e
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-06 15:04:19 -0700

    feat/create Genre component, planning of MovieCard component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import { useState } from 'react';
+import movieData from './mockMovieData.js'
 
 function App() {
 
-  
+
 }
 
 export default App;
--- src/Genre.js
+++ src/Genre.js
@@ -1,7 +1,17 @@
 import './Genre.css';
+import MovieCard from './MovieCard.js';
 
-function Genre() {
-
-}
+const Genre = ({ genre, movies }) => {
+  return (
+    <>
+      <h2>{genre}</h2>
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Genre;
