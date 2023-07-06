a33fa05eaec3f8738f40590f0c70ad62aef08c03
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-06 16:00:49 -0700

    feat/styling for MovieCard section

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import MovieCard from "./MovieCard";
 
 const App = () => {
   return (
-    <>
+    <div className="movie-card-grid">
       {movieData.movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
-    </>
+    </div>
   );
 };
 
