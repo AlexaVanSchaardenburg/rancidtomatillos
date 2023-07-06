70b7cc31dcc02a44664216cb479ebe6caf6496cd
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-06 15:18:22 -0700

    feat/created MovieCard component

--- src/Genre.js
+++ src/Genre.js
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard.js';
 const Genre = ({ genre, movies }) => {
   return (
     <>
-      <h2>{genre}</h2>
+      <h2 className='genre-heading'>{genre}</h2>
       <div className="movie-list">
         {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,7 +1,18 @@
 import './MovieCards.css';
 
-function MovieCards() {
+const MovieCard = ({ movie }) => {
+  const { title, posterImage, releaseDate, averageRating } = movie;
 
-}
+  return (
+    <div className="movie-card">
+      <img className="poster" src={posterImage} alt={title} />
+      <div className="movie-details">
+        <h3 className="movie-title">{title}</h3>
+        <p className="release-date">Year: {releaseDate.split("-")[0]}</p>
+        <p className="movie-rating">Rating: {averageRating}</p>
+      </div>
+    </div>
+  );
+};
 
-export default MovieCards;
+export default MovieCard;
