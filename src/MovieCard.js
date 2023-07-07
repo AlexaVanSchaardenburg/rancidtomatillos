9f7b583b69f86361cfe3025214a5cc2bb992b210
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-07 17:28:15 -0600

    Removed displayDetails and onClick

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,12 +1,12 @@
 import './MovieCard.css';
 
 const MovieCard = ({ movie }) => {
-  const { title, poster_path, release_date, average_rating, displayDetails } = movie;
+  const { title, poster_path, release_date, average_rating } = movie;
 
   const roundedRating = Math.round(average_rating * 10) / 10;
 
   return (
-    <button className="movie-card" onClick={displayDetails}>
+    <button className="movie-card">
       <img className="movie-poster" src={poster_path} alt={title} />
       <div className="movie-details">
         <h2>{title}</h2>
