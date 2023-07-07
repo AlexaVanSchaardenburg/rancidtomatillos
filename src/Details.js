3086fb12a52ddbb39db169a3d3a9dc7fd946e14b
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-07 15:52:28 -0600

    Outlined basic JSX for Details component return

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,28 @@ import './Details.css';
 
 function Details() {
     return (
+        <article>
+            <img>Background Image Here</img>
+            <div>
+                <img>Movie Cover</img>
+                <div>
+                    <h1>Movie Title Here!</h1>
+                    <div>
+                        <p>Release date here</p>
+                        <p>Runtime here</p>
+                    </div>
+                    <p>Tagline for movie</p>
+                    <p>Movie description</p>
+                </div>
+                <div>
+                    <h3>Rating</h3>
+                    <p>Movie Budget:</p>
+                    <p>Budget number here</p>
+                    <p>Movie Revenue:</p>
+                    <p>Movie revenue number here</p>
+                </div>
+            </div> 
+        </article>
     )
 }
 
