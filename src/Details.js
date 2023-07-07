09c2c33cb44c0eacb1984433ef0885ea668e301f
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-07 17:05:44 -0600

    Interpolated all of the movie details in to the Details component

--- src/Details.js
+++ src/Details.js
@@ -1,26 +1,26 @@
 import './Details.css';
 
-function Details() {
+function Details({ movie }) {
     return (
         <article>
-            <img>Background Image Here</img>
+            <img src={movie.backdrop_path} alt={`Backdrop Image for ${movie.title}`} className="backdropImage"></img>
             <div>
-                <img>Movie Cover</img>
+            <img src={movie.poster_path} alt={`Cover Image for ${movie.title}`} className="coverImage"></img>
                 <div>
-                    <h1>Movie Title Here!</h1>
+                    <h1>{movie.title}</h1>
                     <div>
-                        <p>Release date here</p>
-                        <p>Runtime here</p>
+                        <p>{movie.release_date}</p>
+                        <p>{`${movie.runtime} min`}</p>
                     </div>
-                    <p>Tagline for movie</p>
-                    <p>Movie description</p>
+                    <p>{movie.tagline}</p>
+                    <p>{movie.overview}</p>
                 </div>
                 <div>
-                    <h3>Rating</h3>
+                    <h3>{`Rating: ${movie.average_rating}/10`}</h3>
                     <p>Movie Budget:</p>
-                    <p>Budget number here</p>
+                    <p>{`$${movie.budget}`}</p>
                     <p>Movie Revenue:</p>
-                    <p>Movie revenue number here</p>
+                    <p>{`$${movie.revenue}`}</p>
                 </div>
             </div> 
         </article>
