f527a69940d8c7ac33878314da9d829b3e5a3ef7
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-08 17:25:28 -0600

    Moved the api call to the apiCalls file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Nav from "./Nav";
 // import movieData from './mockMovieData.js'
 import individualMovieData from './mockIndividualMovieData.js'
-// import { getAllMovies } from './apiCalls.js'
+import { getAllMovies } from './apiCalls.js'
 import MovieCard from "./MovieCard";
 import Details from './Details.js'
 import { useState, useEffect } from 'react';
@@ -11,33 +11,15 @@ const App = () => {
   const [showMovieDetails, setShowMovieDetails] = useState(false);
   const [allMovies, setAllMovies] = useState([])
 
-  // let allMoviesData;
-
   const handleMovieClick = () => {
     setShowMovieDetails(true);
   };
 
-  const getAllMovies = () => {
-    return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-      .then(response => response.json())
-      .then(data => {
-        setAllMovies(data.movies)
-      })
-      .catch(error => {
-        //code for no repsonse reutned goes here -- ideally do not use alert BUT code below would technically work
-        return alert(`${error.message}`)
-      })
-  };
-
-  // Promise.all(getAllMovies()).then(allMoviesData => {
-  //   setAllMovies(allMoviesData)
-  // })
-
   useEffect(() => {
-    getAllMovies()
+    getAllMovies().then(data => {
+      setAllMovies(data.movies)
+    })
   }, [])
-
-  // console.log(movieData)
 
   return (
     <>
