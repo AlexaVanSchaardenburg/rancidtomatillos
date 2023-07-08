80aea59ea026054330355978a21f1c9780367080
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-08 16:38:52 -0600

    Wrote the fetch request for the movie cards in the App component and got it working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import './App.css';
 import Nav from "./Nav";
 // import movieData from './mockMovieData.js'
-// import individualMovieData from './mockIndividualMovieData.js'
-import { getAllMovies } from './apiCalls.js'
+import individualMovieData from './mockIndividualMovieData.js'
+// import { getAllMovies } from './apiCalls.js'
 import MovieCard from "./MovieCard";
 import Details from './Details.js'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
   const [showMovieDetails, setShowMovieDetails] = useState(false);
+  const [allMovies, setAllMovies] = useState([])
+
+  // let allMoviesData;
 
   const handleMovieClick = () => {
     setShowMovieDetails(true);
   };
 
+  const getAllMovies = () => {
+    return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then(response => response.json())
+      .then(data => {
+        setAllMovies(data.movies)
+      })
+      .catch(error => {
+        //code for no repsonse reutned goes here -- ideally do not use alert BUT code below would technically work
+        return alert(`${error.message}`)
+      })
+  };
 
+  // Promise.all(getAllMovies()).then(allMoviesData => {
+  //   setAllMovies(allMoviesData)
+  // })
+
+  useEffect(() => {
+    getAllMovies()
+  }, [])
+
+  // console.log(movieData)
 
   return (
     <>
@@ -23,7 +46,7 @@ const App = () => {
         <Details movie={individualMovieData.movie} />
       ) : (
         <div className="movie-card-grid">
-          {movieData.movies.map((movie) => (
+          {allMovies.map((movie) => (
             <MovieCard key={movie.id} movie={movie} onClick={handleMovieClick} />
           ))}
         </div>
