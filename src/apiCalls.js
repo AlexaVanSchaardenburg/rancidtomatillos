770e744439ea84dfaf68063e23ea5ba1f50504d8
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-08 15:48:42 -0600

    Set up basic architechture for fetch calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -6,12 +6,39 @@
 //fetch all movies
 
 const getAllMovies = () => {
-
+  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    .then((response) => {
+      if(!response.ok){
+        //code for error handling goes here
+      } else {
+        return response.json()
+      }
+    })
+    .catch(error => {
+      //code for no repsonse reutned goes here -- ideally do not use alert BUT code below would technically work
+      //error => alert(`${error.message}`)
+    })
 };
 
 //fetch specific movie detail
 
-const getIndividualMovie = () => {
+const getIndividualMovie = (id) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+  .then((response) => {
+    if(!response.ok){
+      //code for error handling goes here
+    } else {
+      return response.json()
+    }
+  })
+  .catch(error => {
+    //code for no repsonse reutned goes here -- ideally do not use alert BUT code below would technically work
+    //error => alert(`${error.message}`)
+  })
+};
 
+export {
+  getAllMovies,
+  getIndividualMovie
 };
 
