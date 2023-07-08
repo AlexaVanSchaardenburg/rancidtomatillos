e98485e5c822b7b4b1b4bf8894324894484bf329
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-08 15:43:21 -0600

    Pseudocoded for the fetch calls

---
+++ src/apiCalls.js
@@ -0,0 +1,17 @@
+//on page load fetch all movies and assign those movies to a variables that we can map through to display the movies - potentially use async await?
+  //api calls live in api folder and are exported to needed components
+  // promise.all goes in the component -- to use the data returned in the fetch once it is resolved from the promise. 
+
+
+//fetch all movies
+
+const getAllMovies = () => {
+
+};
+
+//fetch specific movie detail
+
+const getIndividualMovie = () => {
+
+};
+
