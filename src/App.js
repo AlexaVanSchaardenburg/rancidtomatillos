9793bd750edc77a3164abab0c2945cea34f7be91
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-09 13:58:45 -0700

    feat/error handling added to App component with conditional rendering of message

--- src/App.js
+++ src/App.js
@@ -1,42 +1,67 @@
+
 import './App.css';
 import Nav from "./Nav";
-import { getData } from './apiCalls.js'
+import { getData } from './apiCalls.js';
 import MovieCard from "./MovieCard";
-import Details from './Details.js'
+import Details from './Details.js';
 import { useState, useEffect } from 'react';
 
 const App = () => {
   const [showMovieDetails, setShowMovieDetails] = useState(false);
-  const [allMovies, setAllMovies] = useState([])
-  const [individualMovie, setIndividualMovie] = useState([])
+  const [allMovies, setAllMovies] = useState([]);
+  const [individualMovie, setIndividualMovie] = useState([]);
+  const [error, setError] = useState(null);
 
   const handleMovieClick = (id) => {
-    getData(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`).then(data => {
-      setIndividualMovie(data)
-      setShowMovieDetails(true)
-    })
+    getData(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+      .then((data) => {
+        setIndividualMovie(data);
+        setShowMovieDetails(true);
+      })
+      .catch((error) => {
+        setError(error);
+      });
   };
 
   useEffect(() => {
-    getData('https://rancid-tomatillos.herokuapp.com/api/v2/movies').then(data => {
-      setAllMovies(data.movies)
-    })
-  }, [])
+    getData('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then((data) => {
+        setAllMovies(data.movies);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  }, []);
+
   const handleReturnToMain = () => {
     setShowMovieDetails(false);
   };
 
   return (
     <>
-      <Nav showButton={showMovieDetails} onClick={handleReturnToMain} />
-      {showMovieDetails ? (
-        <Details movie={individualMovie.movie} />
+      {error ? (
+        <>
+          <Nav showButton={showMovieDetails} onClick={handleReturnToMain} />
+          <div className='error-message'>Error: {error.message}</div>
+        </>
       ) : (
-        <div className="movie-card-grid">
-          {allMovies.map((movie) => (
-            <MovieCard key={movie.id} id={movie.id} movie={movie} onClick={() => handleMovieClick(movie.id)} />
-          ))}
-        </div>
+        <>
+          <Nav showButton={showMovieDetails} onClick={handleReturnToMain} />
+          {showMovieDetails ? (
+            <Details movie={individualMovie.movie} />
+          ) : (
+            <div className="movie-card-grid">
+              {allMovies.map((movie) => (
+                <MovieCard
+                  key={movie.id}
+                  id={movie.id}
+                  movie={movie}
+                  onClick={() => handleMovieClick(movie.id)}
+                />
+              ))}
+            </div>
+          )}
+        </>
       )}
     </>
   );
