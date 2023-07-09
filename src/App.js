259a1bbb04fcc6044e01229b84ada2760d98e3d2
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-08 20:22:20 -0600

    Got the fetch request for individual movies

--- src/App.js
+++ src/App.js
@@ -1,52 +1,42 @@
 import './App.css';
 import Nav from "./Nav";
-// import movieData from './mockMovieData.js'
-import individualMovieData from './mockIndividualMovieData.js'
 import { getData } from './apiCalls.js'
 import MovieCard from "./MovieCard";
 import Details from './Details.js'
 import { useState, useEffect } from 'react';
 
 const App = () => {
-  const [movieId, setMovieId] = useState(null)
   const [showMovieDetails, setShowMovieDetails] = useState(false);
   const [allMovies, setAllMovies] = useState([])
   const [individualMovie, setIndividualMovie] = useState([])
 
   const handleMovieClick = (id) => {
-    setMovieId(id)
-    console.log(movieId);
-    getData(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movieId}`).then(data => {
+    getData(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`).then(data => {
       setIndividualMovie(data)
+      setShowMovieDetails(true)
     })
-    // })
-    setShowMovieDetails(true)
   };
 
   useEffect(() => {
     getData('https://rancid-tomatillos.herokuapp.com/api/v2/movies').then(data => {
       setAllMovies(data.movies)
     })
-    // getData(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270`).then(data => {
-    //   setIndividualMovie(data)
-    // })
   }, [])
 
   return (
     <>
       <Nav />
       {showMovieDetails ? (
-        <Details movie={individualMovieData.movies} />
+        <Details movie={individualMovie.movie} />
       ) : (
         <div className="movie-card-grid">
           {allMovies.map((movie) => (
-            <MovieCard key={movie.id} id={movie.id} movie={movie} onClick={handleMovieClick} />
+            <MovieCard key={movie.id} id={movie.id} movie={movie} onClick={() => handleMovieClick(movie.id)} />
           ))}
         </div>
       )}
     </>
   );
 };
-
 
 export default App;
