38be90aceeec9a66825d16e649986d17fb710c9d
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-08 19:56:38 -0600

    Outlined way to fetch data for individual movie

--- src/App.js
+++ src/App.js
@@ -8,28 +8,39 @@ import Details from './Details.js'
 import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [movieId, setMovieId] = useState(null)
   const [showMovieDetails, setShowMovieDetails] = useState(false);
   const [allMovies, setAllMovies] = useState([])
+  const [individualMovie, setIndividualMovie] = useState([])
 
-  const handleMovieClick = () => {
-    setShowMovieDetails(true);
+  const handleMovieClick = (id) => {
+    setMovieId(id)
+    console.log(movieId);
+    getData(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movieId}`).then(data => {
+      setIndividualMovie(data)
+    })
+    // })
+    setShowMovieDetails(true)
   };
 
   useEffect(() => {
     getData('https://rancid-tomatillos.herokuapp.com/api/v2/movies').then(data => {
       setAllMovies(data.movies)
     })
+    // getData(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270`).then(data => {
+    //   setIndividualMovie(data)
+    // })
   }, [])
 
   return (
     <>
       <Nav />
       {showMovieDetails ? (
-        <Details movie={individualMovieData.movie} />
+        <Details movie={individualMovieData.movies} />
       ) : (
         <div className="movie-card-grid">
           {allMovies.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} onClick={handleMovieClick} />
+            <MovieCard key={movie.id} id={movie.id} movie={movie} onClick={handleMovieClick} />
           ))}
         </div>
       )}
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,37 +1,3 @@
-//fetch all movies
-
-const getAllMovies = () => {
-  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-    .then((response) => {
-      if(!response.ok){
-        //code for error handling goes here
-      } else {
-        return response.json()
-      }
-    })
-    .catch(error => {
-      //code for no repsonse reutned goes here -- ideally do not use alert BUT code below would technically work
-      //error => alert(`${error.message}`)
-    })
-};
-
-//fetch specific movie detail
-
-const getIndividualMovie = (id) => {
-  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-  .then((response) => {
-    if(!response.ok){
-      //code for error handling goes here
-    } else {
-      return response.json()
-    }
-  })
-  .catch(error => {
-    //code for no repsonse reutned goes here -- ideally do not use alert BUT code below would technically work
-    //error => alert(`${error.message}`)
-  })
-};
-
 const getData = (url) => {
   return fetch(url)
   .then((response) => {
