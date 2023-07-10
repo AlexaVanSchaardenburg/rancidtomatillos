807f18b88bffb567935f00fa3034524ef51584b9
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-10 16:59:41 -0600

    Set up the beofreEach hook with the skeleton of intercepts for all three fetch requests used in the test

--- cypress/e2e/details.cy.js
+++ cypress/e2e/details.cy.js
@@ -1,15 +1,59 @@
-describe('template spec', () => {
+describe('Test that user can click on a movie to view more details', () => {
 
   //intercept the fetch call for all movies
-  //intercept the fetch call for a specific movie
+beforeEach(() => {
+  //FOR ALL MOVIES
+  cy.intercept('GET', 'url for all movies', {
+    statusCode: 200,
+    body: {
+      //mock data here or figure out how to use a fixture
+    }
+  });
+//FOR SPECIFIC MOVIE #1
+  cy.intercept('GET', 'url for specific movie', {
+    statusCode: 200,
+    body: {
+      //mock data here or figure out how to use a fixture
+    }
+  });
+//FOR SPECIFIC MOVIE #2
+  cy.intercept('GET', 'url for other specific movie', {
+    statusCode: 200,
+    body: {
+      //mock data here or figure out how to use a fixture
+    }
+  });
+//VISIT THE PAGE
+  cy.visit('http://localhost:3000/')
+})
 
-  it('Should show the user movie detials', () => {
-    cy.visit('http://localhost:3000/')
+  it('Should show the user movie details', () => {
       // click the first movie -- put the movie card id as the get parameter
-    // cy.get().click()
+    cy.get().click()
       //check that the nav is there and has the title image and the home button
     cy.get('.nav-container').contains('img')
       //check that there is a backgorund photo
       //check that there is the cover photo, title, year, runtime, tagline, description, rating, budget, budget number, revenue, revenue number
   })
+  it("should show the user details for a diifferent movie", () => {
+    
+  })
+  it("Should display an informational error message if the fetch call fails (500)", () => {
+    
+  })
+  it("Should display an informational error message if the client fails to make the fetch call (400)", () => {
+    
+  })
 })
+
+//TO DO TO WRITE TESTING
+  //Set up mock data file
+    //figure out how to use Postman to view the data
+    //set up mock data in fixtures files
+  //Set up intercepts for both fetch calls
+    //use mock data from 
+    //ffigure out how to use fixtures
+  //Write test to check that all the info I expect to be there is there for movie #1
+  //Write same test for another movie???
+  //Write sad path to test that I get the expected errors if the fetch fails 
+    //one test for 400, one for 500
