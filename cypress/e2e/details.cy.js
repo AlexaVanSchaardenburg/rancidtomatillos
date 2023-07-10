ede5ddd042c6a4e3df1865ee31f9d242cc17fdbb
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-10 16:42:17 -0600

    Outlined the format for testing a click on a movie to view details

--- cypress/e2e/details.cy.js
+++ cypress/e2e/details.cy.js
@@ -2,8 +2,14 @@ describe('template spec', () => {
 
   //intercept the fetch call for all movies
   //intercept the fetch call for a specific movie
-  
-  it('passes', () => {
+
+  it('Should show the user movie detials', () => {
     cy.visit('http://localhost:3000/')
+      // click the first movie -- put the movie card id as the get parameter
+    // cy.get().click()
+      //check that the nav is there and has the title image and the home button
+    cy.get('.nav-container').contains('img')
+      //check that there is a backgorund photo
+      //check that there is the cover photo, title, year, runtime, tagline, description, rating, budget, budget number, revenue, revenue number
   })
 })
