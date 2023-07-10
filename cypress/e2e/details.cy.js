e6ce0eea34f48af1af5150d9c37a6471ed3cd420
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-10 15:54:17 -0600

    Created file for details component testing

---
+++ cypress/e2e/details.cy.js
@@ -0,0 +1,9 @@
+describe('template spec', () => {
+
+  //intercept the fetch call for all movies
+  //intercept the fetch call for a specific movie
+  
+  it('passes', () => {
+    cy.visit('http://localhost:3000/')
+  })
+})
