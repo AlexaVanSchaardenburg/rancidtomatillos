f6d7119237991ed96a0142a1c7ee6b2595e1ba95
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-12 17:27:37 -0600

    Added informational comments to the index.js file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//to set up the routing I installed the react-router-dom and then importd teh browser router below
 import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  //The whole app is now wrapped in BrowserRouter which allows us to use router to set up navigation in the app
   <BrowserRouter>
     <React.StrictMode>
       <App />
