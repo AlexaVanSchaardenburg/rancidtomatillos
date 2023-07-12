7060ac3850e63b4bea2aef2cc5608d2ec7044045
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-11 22:55:59 -0600

    Set up the Routes componet and git status

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { getData } from './apiCalls.js';
 import MovieCard from "./MovieCard";
 import Details from './Details.js';
 import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   const [showMovieDetails, setShowMovieDetails] = useState(false);
@@ -37,34 +38,63 @@ const App = () => {
     setShowMovieDetails(false);
   };
 
+  const allMoviesView = (movies) => {
+    return (
+      <div className="movie-card-grid">
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            id={movie.id}
+            movie={movie}
+            onClick={() => handleMovieClick(movie.id)}
+          />
+        ))}
+      </div>
+    )
+  }
+
   return (
     <>
-      {error ? (
-        <>
-          <Nav showButton={showMovieDetails} onClick={handleReturnToMain} />
-          <div className='error-message'>Error: {error.message}</div>
-        </>
-      ) : (
-        <>
-          <Nav showButton={showMovieDetails} onClick={handleReturnToMain} />
-          {showMovieDetails ? (
-            <Details movie={individualMovie.movie} />
-          ) : (
-            <div className="movie-card-grid">
-              {allMovies.map((movie) => (
-                <MovieCard
-                  key={movie.id}
-                  id={movie.id}
-                  movie={movie}
-                  onClick={() => handleMovieClick(movie.id)}
-                />
-              ))}
-            </div>
-          )}
-        </>
-      )}
+      <Nav />
+      <Routes >
+        <Route path="/" element={allMoviesView(allMovies)} />
+        {/* <Route path="/:id" element={} /> */}
+      </Routes>
     </>
-  );
+  )
+    //need to return the Routes - do I need to move other code out?
+    //
+  //   <>
+  //     {error ? (
+  //       <>
+  //         <Nav showButton={showMovieDetails} onClick={handleReturnToMain} />
+  //         <div className='error-message'>Error: {error.message}</div>
+  //       </>
+  //     ) : (
+  //       <>
+  //         <Nav showButton={showMovieDetails} onClick={handleReturnToMain} />
+  //         {showMovieDetails ? (
+  //           <Details movie={individualMovie.movie} />
+  //         ) : (
+  //           <div className="movie-card-grid">
+  //             {allMovies.map((movie) => (
+  //               <MovieCard
+  //                 key={movie.id}
+  //                 id={movie.id}
+  //                 movie={movie}
+  //                 onClick={() => handleMovieClick(movie.id)}
+  //               />
+  //             ))}
+  //           </div>
+  //         )}
+  //       </>
+  //     )}
+  //   </>
+  // );
+
+
+
+
 };
 
 export default App;
