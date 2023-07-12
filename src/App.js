63adb79e3ec108d22c7bd4bf5732ae8d6b4c5cb1
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-12 17:43:53 -0600

    Added informational comments in the App file

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import { getData } from './apiCalls.js';
 import MovieCard from "./MovieCard";
 import Details from './Details.js';
 import { useState, useEffect } from 'react';
+//In order to define all the paths that will be used by the app I imported Routes and Route below. The Routes component will now be a part of the App's return statment.
 import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
+  //I removed a lot of the state that delt with conditonal rendering and any state for the specific movie that is rendered on the details page has moved to the details component
   const [allMovies, setAllMovies] = useState([]);
   const [error, setError] = useState(null);
+
+  //The functions for calling the specific movie data, and for handling any clicks were removed from here as routing takes care of the clicks and the fetch has been moved into the details component. 
 
   useEffect(() => {
     getData('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
@@ -22,6 +26,7 @@ const App = () => {
   }, []);
 
   const allMoviesView = (movies) => {
+    //This function is really just to make the retun more readable, but is the part of the app component that takes all of the fetched movies and maps them into rendered movie cards - this function is called in the return statement so the data is rendered on page load just like it was before.
     return (
       <div className="movie-card-grid">
         {movies.map((movie) => (
@@ -38,13 +43,12 @@ const App = () => {
   return (
     <>
       <Nav />
+      //The routes component below defines all of the routes in the app
       <Routes >
+        //The route below is the home page and the default for the app to start on. The element for the home page is the invocation of the mapping functiona above and displays all the movie cards - important that it's path is just the /
         <Route path="/" element={allMoviesView(allMovies)} />
+        //The route below this is the path for the details page - the element is the Details component which no longer needs any props, and the path is dynamic with anything after the colon beign dynamic. In this case the id is determined in the MovieCard componenet and is explained more there.
         <Route path="/:id" element={<Details />} />
-        //I think the async js is causing the error - aka route runs before the fetch come back and then we get a runtime error
-        //need to dig into how the fetch was working before hand to better understadn hwo I prevented that and then apply that to the link
-        //isnt working because the link click has immediate effects and the fetch doesn't have time
-        //potential fix = put the fetch in the details component - have the component render a loading symbol of some kind - then when the fetch returns replace that loading symbol with the data
       </Routes>
     </>
   )
