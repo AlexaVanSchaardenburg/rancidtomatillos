1f464879eca1c0cc99873be4bed18f7274b389e6
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-13 17:26:38 -0700

    refactor/routes tests so they no longer fetch. All are being stubbed

--- cypress/e2e/routes.cy.js
+++ cypress/e2e/routes.cy.js
@@ -1,4 +1,5 @@
 import moviesData from "../fixtures/moviesData";
+import { movie1Data, movie2Data } from "../fixtures/movieData";
 
 describe('Check routes for navigation to MovieDetails page via MovieCard click', () => {
     beforeEach(() => {
@@ -14,12 +15,22 @@ describe('Check routes for navigation to MovieDetails page via MovieCard click',
 
   it('should navigate to the correct route path when the 1st card is clicked', () => {
 
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270', {
+      statusCode: 200,
+      body: movie1Data
+    });
+
     cy.get('.movie-card').first().click();
 
     cy.url().should('eq', 'http://localhost:3000/436270');
   })
 
   it('should navigate to the correct route path when the last card is clicked', () => {
+
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/1013860', {
+      statusCode: 200,
+      body: movie2Data
+    });
 
     cy.get('.movie-card').last().click();
 
